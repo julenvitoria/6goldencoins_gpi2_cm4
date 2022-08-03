d890e243694cf0cd27471b0f7af9522ed8638e7d
julenvitoria <julenvitoria@users.noreply.github.com>
2022-08-03 11:07:28 +0200

    bezelsgpi.sh

--- temporal/bezelsgpi.sh
+++ temporal/bezelsgpi.sh
@@ -21,8 +21,9 @@ case $menuitem in
     #Buscamos y borramos todos los cfg en /roms para despues borrarlos, una vez borrados se procedera a copiar los seleccionados
     find /home/pi/RetroPie/roms/ -type f -name "*.cfg" -exec rm -f {} \;
     find /opt/retropie/configs/ -type f \( -name "retroarch.cfg" -a -not -wholename "*pc/retroarch.cfg" -wholename "*gameandwatch/retroarch.cfg" -wholename "*dreamcast/retroarch.cfg" -wholename "*all/retroarch.cfg" \)  -exec rm -f {} \;
-
-    cp /home/pi/RetroPie/script/atari5200/retroarch-1080.cfg /opt/retropie/configs/atari5200/retroarch.cfg
+    cd /home/pi/scripts/bezelsconfig
+    cp -R * /opt/retropie/configs/
+    #cp /home/pi/RetroPie/script/atari5200/retroarch-1080.cfg /opt/retropie/configs/atari5200/retroarch.cfg
     sleep 0.5
     dialog --infobox "
 
